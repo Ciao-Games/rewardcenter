cf975991ee381a961d6ff6156d1984c635529205
Nikos Giannaras <nikos@ciao.games>
2026-09-07 15:32:30 +0300

    Copy Config file and test data menu items.

--- Runtime/RewardCenterConstants.cs
+++ Runtime/RewardCenterConstants.cs
@@ -8,12 +8,18 @@ namespace Ciao.RC
         public const string CurrencyIconFilename = "currency_icon";
         public const string AssetsFolder = "rewardcenter/assets";
         public const string ConfigResourceName = "RewardCenterConfig";
+        public const string TestDataFileName = "RewardCenterTestData";
         
         // HTTP
         public const string QueryCampaignId = "cid";
         public const string QueryPayload = "payload";
         public const string QueryConfigUrl = "config_url";
         public const int HttpTimeoutSeconds = 5;
+        
+        // Assets
+        public const string PrefabsSourceDir = "Packages/com.ciaogames.rewardcenter/Runtime/Prefabs";
+        public const string PrefabsDestDir = "Assets/RewardCenter/Prefabs";
+        public const string ResourcesDir = "Assets/RewardCenter/Resources";
         
     }
 }
